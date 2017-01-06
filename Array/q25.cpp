c1ed7addb2a05b198722dfa492a67f55c9935cfe
BitForceStudio <bitforce.studio@gmail.com>
2017-01-06 23:06:33 +0000

    one hard question
    finished it by debugging and trying…

---
+++ Array/q25.cpp
@@ -0,0 +1,99 @@
+/****************************************************************************************************
+                                   25. Reverse Nodes in k-Group          
+-----------------------------------------------------------------------------------------------------
+Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
+
+k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes in the end should remain as it is.
+
+You may not alter the values in the nodes, only nodes itself may be changed.
+
+Only constant memory is allowed.
+
+For example,
+Given this linked list: 1->2->3->4->5
+
+For k = 2, you should return: 2->1->4->3->5
+
+For k = 3, you should return: 3->2->1->4->5
+****************************************************************************************************/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if (head==NULL || head->next==NULL || k==1) return head;
+        
+        ListNode* dphead=new ListNode(0);
+        dphead->next=head; 
+        ListNode* cphead = dphead;
+        ListNode* kth = head;
+        
+        // first k, in order to get head
+        int j=0;
+        while(j<k && kth!=NULL)
+        {
+            kth=kth->next;
+            j++;
+        }
+        if (j<k && kth==NULL) return cphead->next;
+        
+        ListNode* tmp = dphead;
+        ListNode* ntmp = dphead; ntmp=ntmp->next->next;
+        dphead=dphead->next; dphead->next=kth;
+
+        for(int i=1;i<k;i++)
+        {
+            tmp = ntmp->next;
+            ntmp->next=dphead;
+            dphead=ntmp;
+            ntmp=tmp;
+        }
+        
+        // get head
+        cphead->next = dphead;
+        
+        for(int i=1;i<k;i++) dphead=dphead->next;
+        
+        while(1)
+        {
+            kth = dphead;  
+            j=0;
+            while(j<k && kth!=NULL)
+            {
+                kth=kth->next;
+                j++;
+            }
+            if (kth==NULL) break;
+            
+            dphead->next=kth; 
+            dphead = tmp;
+            ntmp=tmp->next;
+            for(int i=1;i<k;i++)
+            {
+                tmp = ntmp->next;
+                ntmp->next=dphead;
+                dphead=ntmp;
+                ntmp=tmp;
+            }
+            
+            for(int i=1;i<k;i++) dphead=dphead->next;
+            dphead->next=tmp;
+        }
+        
+        
+        return cphead->next;
+    }
+};
+
+/****************************************************************************************************
+                                             Note
+finished this by trying and debugging. the idea is simple. three pointers, copy the ->next->next ,
+reverse the curr and ->next, then move all to prepare next loop.
+****************************************************************************************************/
