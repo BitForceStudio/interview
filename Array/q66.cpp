e69c9dc52d5913cc6fd0b3bcc406acccbeadf19f
BitForceStudio <BitForce.Studio@gmail.com>
2017-01-12 13:24:28 +0000

    added a c++ project to hold the problems
    added a project
    start to change names.
    ** remember to review the problem frequently.

--- Array/q66.cpp
+++ Array/q66.cpp
@@ -7,38 +7,42 @@ You may assume the integer do not contain any leading zero, except the number 0 
 
 The digits are stored such that the most significant digit is at the head of the list.
 ****************************************************************************************************/
+#include "problems\problems\Header.h"
 
-class Solution {
-public:
+namespace std
+{
+  class Solution{
+  public:
     vector<int> plusOne(vector<int>& digits) {
-        if (digits[0]==0) 
-        {
-            digits[0]=1; 
-            return digits;
-        }
-        
-        int len = digits.size();
-        for(int i=len-1;i>=0;i--)
-        {
-            int rst=digits[i]+1;
-            if (rst<=9) 
-            {
-                digits[i] = rst;
-                break;
-            }
-            else if (rst>9 && i>0)
-            {
-                digits[i]=0;
-            }
-            else if (rst>9 && i==0)
-            {
-                digits[0]=1;
-                digits.push_back(0);
-            }
-        }
+      if (digits[0] == 0)
+      {
+        digits[0] = 1;
         return digits;
+      }
+
+      int len = digits.size();
+      for (int i = len - 1; i >= 0; i--)
+      {
+        int rst = digits[i] + 1;
+        if (rst <= 9)
+        {
+          digits[i] = rst;
+          break;
+        }
+        else if (rst > 9 && i > 0)
+        {
+          digits[i] = 0;
+        }
+        else if (rst > 9 && i == 0)
+        {
+          digits[0] = 1;
+          digits.push_back(0);
+        }
+      }
+      return digits;
     }
-};
+  };
+}
 
 /****************************************************************************************************
                                              Note
