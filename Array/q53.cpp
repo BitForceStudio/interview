851e743facfccd39ee659ae6131c28fa76ad58e7
BitForceStudio <bitforce.studio@gmail.com>
2017-04-04 21:17:52 +0100

    20170404
    start to review all the questions

--- Array/q53.cpp
+++ Array/q53.cpp
@@ -33,12 +33,55 @@ public:
     }
 };
 
+class BetterLinearSolution {
+public:
+    int maxSubArray(vector<int>& nums){
+        int len=nums.size();
+        if(len<1) return 0;
+        int currmax = nums[0];
+        int allmax  = nums[0];
+        for(int i=1;i<len;i++){
+            if (currmax<0) currmax=0;
+            currmax+=nums[i];
+            allmax=allmax>currmax?allmax:currmax;
+        }
+        return allmax;
+    }
+};
+
+class D_C_Solution{
+public:
+    int maxSubArray(vector<int>& nums){
+        int len=nums.size();
+        if(len<1) return 0;
+        return helper(0,len-1,nums);  
+    }
+
+    int helper(int st,int ed,vector<int> nums){
+        if(st==ed) return nums[st];
+        int mid=(st+ed)/2;
+        int leftmax = helper(st,mid,nums);
+        int rightmax= helper(mid+1,ed,nums);
+        int currmax = nums[mid];
+        int currsum = nums[mid];
+
+        for(int i=mid-1;i>=0;i--){
+            currsum+=nums[i];
+            if(currsum>currmax) currmax=currsum; 
+        }  
+        currsum=currmax;
+        for(int i=mid+1;i<=ed;i++){
+            currsum+=nums[i];
+            if(currsum>currmax) currmax=currsum; 
+        }
+        return max(currmax,max(leftmax,rightmax));
+    } 
+};
+
 /****************************************************************************************************
                                              Note
-if all negtive, need to get the largest. 
+Linear solution: initial the max to be the first element. In the loop compare the the current max
+with 0 first, then added the current element. Then compare the current max. 
 
-divide and conquer solution? 
-
-middle: the max sum solutiom must include the middle points. so, it will be linear problem. expand on
-both ways and sum the max. 
+divide and conquer: remember the end element should be considered. 
 ****************************************************************************************************/
