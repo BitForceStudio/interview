29a4666bf95e8d1da3ed5e91c1c2da2352be7bea
BitForceStudio <bitforce.studio@gmail.com>
2017-09-18 23:38:45 +0100

    update the solutions

--- Array/q110.cpp
+++ Array/q110.cpp
@@ -16,25 +16,50 @@ the two subtrees of every node never differ by more than 1.
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-class Solution {
-public:
-    bool isBalanced(TreeNode* root) {
-        if (root == NULL) return true;
-        int rst = htree(root);
-        return rst!=-1;
-    }
-    
-    int htree(TreeNode* nt)
-    {
-        if (nt==NULL) return 0;
-        int left = htree(nt->left);  // here should be +1.
-        int right=htree(nt->right);
-        
-        if (left!=-1 && right != -1 && abs(left-right)<=1) return max(left,right)+1;
-        else return -1;
-    }
-};
+#include "problems\problems\Header.h"
 
+namespace std
+{
+    class Solution {
+    public:
+        bool isBalanced(TreeNode* root) {
+            if (root == NULL) return true;
+            int rst = htree(root);
+            return rst!=-1;
+        }
+        
+        int htree(TreeNode* nt)
+        {
+            if (nt==NULL) return 0;
+            int left = htree(nt->left);  // here should be +1.
+            int right=htree(nt->right);
+            
+            if (left!=-1 && right != -1 && abs(left-right)<=1) return max(left,right)+1;
+            else return -1;
+        }
+    };
+
+    class Solution2 {
+    public:
+        bool isBalanced(TreeNode* root) {
+            if (root==NULL) return true;
+            
+            return helper(root)>=0;
+        }
+        
+        int helper(TreeNode* root)
+        {
+            if (root==NULL) return 0;
+            int dleft = helper(root->left);
+            if (dleft==-1) return -1;
+            int dright = helper(root->right);
+            if (dright==-1) return -1;
+            if (abs(dleft-dright)>1) return -1;
+            
+            return (dleft>dright?dleft:dright)+1;
+        }
+    };
+}
 /****************************************************************************************************
                                              Note
 just remember do not plus 1 on that place. or it wont be -1...
--- Array/q3_LgNoRept.cpp
+++ Array/q3_LgNoRept.cpp
@@ -35,10 +35,43 @@ namespace std
       return maxlen;
     }
   };
+
+  class Solution2 {
+  public:
+      int lengthOfLongestSubstring(string s) {
+          int len=s.size();
+          if (len<2) return len;
+          
+          vector<int> counter(256,0);
+          int rst=0;
+          int curr=0;
+          int start=0;
+          for(int i=0;i<len;i++)
+          {
+              if (counter[s[i]]>0)
+              {
+                  rst=rst>curr?rst:curr;
+                  int tmp = counter[s[i]];
+                  for(int j=start;j<counter[s[i]];j++)
+                  {
+                      counter[s[j]]=0;
+                      curr--;
+                  }
+                  start = tmp;
+              }
+              counter[s[i]]=i+1;
+              curr++;
+          }
+          return rst>curr?rst:curr;
+      }
+  };
 }
 /****************************************************************************************************
                                              Note
 Hint: use the so-called hash table. default as false, two pointers, one is faster and one is slower.
 if faster find the repeating, slower move to the repeat ones and set all chars before repeat to be 
 false.
+
+Solution 2: it is the current me understandable solution. 
+becareful about this question, need to remember the catches. 
 ****************************************************************************************************/
